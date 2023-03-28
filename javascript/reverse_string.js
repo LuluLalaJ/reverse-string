aa559ddb25d56d8608caf28da6bea827eee2a37c
Chen Jiang <chen.jiang.1206@gmail.com>
2023-03-27 20:31:05 -0400

    reverse using loop

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,5 +1,16 @@
 function reverseString(str) {
   // type your code here
+  if (str === "" ) {
+    return ""
+  } else if (str.length === 1) {
+    return str
+  } else {
+    let newStr = "";
+    for (let i = str.length-1; i >= 0; i--) {
+      newStr += str[i]
+    }
+    return newStr
+  }
 }
 
 if (require.main === module) {
